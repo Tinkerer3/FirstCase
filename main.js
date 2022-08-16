edec2c1ce650ae457188024d6d07029ef57e4469
Adem Yıldırım <91284855+Tinkerer3@users.noreply.github.com>
2022-08-16 17:41:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function oddishOrEvenish(Int){
+
+    let arrayFunc = num => Number(num); // verilen sayıyının basamaklarındaki rakamları ayırıp diziye aktarır
+    var intArr = Array.from(String(Int), arrayFunc); // üst satırdaki fonksiyonu kullanır
+    if(intArr.length == "1"){
+        if(Int%2==0){
+            console.log("Even");
+        } // eğer sayı tek rakamlıysa o rakamın iki ile bölümünü alıp tek ya da çift olmasını kontrol eder
+        else if(Int%2==1){
+            console.log("Odd");
+        }
+    }
+    else if(intArr.length != "1"){
+        Int = intArr.reduce((partialSum, a) => partialSum + a, 0); // eğer sayı birden fazla rakam içeriyorsa rakamları tek tek toplayıp Int değerini ona çevirir.
+        if(Int%2==0){
+            console.log("Even");
+        }
+        else if(Int%2==1){
+            console.log("Odd");
+        }
+}
+}
+
+oddishOrEvenish(43)
+oddishOrEvenish(373)
+oddishOrEvenish(4433)
